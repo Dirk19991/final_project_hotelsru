5dd05541699eca00b179e00db0a1974f995dcb2a
khanchenkov <daniilkhanchenkov@gmail.com>
2023-04-13 14:52:01 +0300

    Добавил сброс, шрифт, небольшие помарки

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,20 @@
 import '@/styles/globals.scss'
+import '@/styles/vars.scss'
 import type { AppProps } from 'next/app'
-import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({
+    weight: ['100', '300', '400', '500', '700'],
+    style: ['normal', 'italic'],
+    subsets: ['latin'],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
             <style jsx global>{`
                 html {
-                    font-family: ${inter.style.fontFamily};
+                    font-family: ${roboto.style.fontFamily};
                 }
             `}</style>
             <Component {...pageProps} />
