b5ca786759aa0ed6574225d95a608efef4316427
khanchenkov <daniilkhanchenkov@gmail.com>
2023-04-13 17:46:05 +0300

    добавил тесты, добавил пакеты для иконок, переменные

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,39 +3,99 @@ import Image from 'next/image'
 import styles from './Header.module.scss'
 import { Button } from '@/stories/Button/ButtonStandard'
 import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+const { wrapper, content, header, navigation, menu, panel, logo, search } =
+    styles
 
 const Header = () => {
     return (
-        <header className={styles.header}>
+        <header className={header}>
             <div className="container">
-                <div className={styles.wrapper}>
-                    <nav>
-                        <Link href="/">
-                            <Image
-                                className="header__logo"
-                                src="https://solea-parent.dfs.ivi.ru/picture/ea003d,ffffff/reposition_iviLogoPlateRounded.svg"
-                                alt="ivi"
-                                width={66}
-                                height={48}
-                            />
-                        </Link>
-                        <ul>
-                            <li>Мой Иви</li>
-                            <li>Что нового</li>
-                            <li>Фильмы</li>
-                            <li>Сериалы</li>
-                            <li>Мультфильмы</li>
-                            <li>TV+</li>
-                        </ul>
-                    </nav>
-                    <div>
-                        <Button
-                            href="/"
-                            label="Смотреть по подписке"
-                            type="watchSubscription"
-                        ></Button>
-                        <div>Поиск</div>
+                <div className={wrapper}>
+                    <div className={content}>
+                        <nav className={navigation}>
+                            <Link href="/" className={logo}>
+                                <Image
+                                    src="https://solea-parent.dfs.ivi.ru/picture/ea003d,ffffff/reposition_iviLogoPlateRounded.svg"
+                                    alt="ivi"
+                                    width={66}
+                                    height={48}
+                                />
+                            </Link>
+                            <ul role="list" className={menu}>
+                                <li>
+                                    <Link
+                                        href="https://www.ivi.ru/"
+                                        title="Мой Иви"
+                                    >
+                                        Мой Иви
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        href="https://www.ivi.ru/new"
+                                        title="Мой Иви"
+                                    >
+                                        Что нового
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        href="https://www.ivi.ru/movies"
+                                        title="Мой Иви"
+                                    >
+                                        Фильмы
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        href="https://www.ivi.ru/series"
+                                        title="Мой Иви"
+                                    >
+                                        Сериалы
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        href="https://www.ivi.ru/animation"
+                                        title="Мой Иви"
+                                    >
+                                        Мультфильмы
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        href="https://www.ivi.ru/tvplus"
+                                        title="Мой Иви"
+                                    >
+                                        TV+
+                                    </Link>
+                                </li>
+                            </ul>
+                        </nav>
+                        <div className={panel}>
+                            <Button
+                                href="/"
+                                label="Смотреть 30 дней бесплатно"
+                                type="watchSubscription"
+                            ></Button>
+                            <div className={search}>
+                                <button>
+                                    <div>
+                                        <FontAwesomeIcon
+                                            icon={faMagnifyingGlass}
+                                            size="xl"
+                                        />
+                                        Поиск
+                                    </div>
+                                </button>
+                            </div>
+                            <div className="notify">колокольчик</div>
+                            <div className="avatar">аватар</div>
+                        </div>
                     </div>
+                    <div className="dropdownBody"></div>
                 </div>
             </div>
         </header>
---
+++ src/tests/components/Header.test.tsx
@@ -0,0 +1,11 @@
+import Header from '@/components/Header/Header'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+describe('Testing Header Component', () => {
+    it('should render properly', () => {
+        render(<Header />)
+        const navigationBar = screen.getByRole('list')
+        expect(navigationBar).toBeInTheDocument()
+    })
+})
