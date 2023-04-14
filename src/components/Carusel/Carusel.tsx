9de7aa639034a97c031b32528842a9b86a23da2a
Dmitry Khodyrev <dooommm@mail.ru>
2023-04-14 17:02:06 +0800

    перенес слайд в сторибук, добавил 5 слайдов и адаптивность слайдеру

--- src/components/Carusel/Carusel.tsx
+++ src/components/Carusel/Carusel.tsx
@@ -3,15 +3,14 @@ import styles from './Carusel.module.scss'
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/scss'
-import Slide from './Slide/Slide'
 import 'swiper/css/bundle'
+import Slide from '@/stories/Slide/Slide'
 
 interface Props {
     slides: Array<any> // прописать типы
 }
 
 const Carusel = () => {
-
     // const listArray = slides.map((slide, index) => {
     //     return (
     //         <SwiperSlide key={index}>
@@ -29,20 +28,43 @@ const Carusel = () => {
                         spaceBetween={50}
                         slidesPerView={1}
                         loop={true}
-                        autoplay={{delay: 3000}}
+                        autoplay={{ delay: 3000 }}
                         navigation
                     >
                         <SwiperSlide>
-                            <Slide />
+                            <Slide
+                                mainImage="/slides/1/main.jpg"
+                                mainText="Сотрудник ФСБ с тёмным прошлым защищает свою страну. Детектив с Сергеем Безруковым"
+                                titleImage="/slides/1/title.png"
+                            />
                         </SwiperSlide>
                         <SwiperSlide>
-                            <Slide />
+                            <Slide
+                                mainImage="/slides/2/main.jpg"
+                                mainText="Ваши любимые Джейсон Стэйтем и Хью Грант спасают мир в шпионской комедии вашего любимого Гая Ричи"
+                                titleImage="/slides/2/title.png"
+                            />
                         </SwiperSlide>
                         <SwiperSlide>
-                            <Slide />
+                            <Slide
+                                mainImage="/slides/3/main.jpg"
+                                mainText="Кай и Герда находят новый друзей и вновь спасают мир. Долгожданное возвращение любимых героев"
+                                titleImage="/slides/3/title.png"
+                            />
                         </SwiperSlide>
                         <SwiperSlide>
-                            <Slide />
+                            <Slide
+                                mainImage="/slides/4/main.jpg"
+                                mainText="Дети пытаются помирить поссорившихся родителей. Семейная комедия с Сергеем Жуковым и Helen Yes"
+                                titleImage="/slides/4/title.png"
+                            />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <Slide
+                                mainImage="/slides/5/main.jpg"
+                                mainText="  Успешный юрист пытается спасти сына у опасной черты. Хью Джекман в мощной драме от режиссёра «Отца»"
+                                titleImage="/slides/5/title.png"
+                            />
                         </SwiperSlide>
                     </Swiper>
                 </div>
---
+++ src/stories/Slide/Slide.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import Slide from './Slide'
+import '../../styles/globals.scss'
+
+const meta: Meta<typeof Slide> = {
+    title: 'Слайд',
+    component: Slide,
+    tags: ['autodocs'],
+}
+
+export default meta
+type Story = StoryObj<typeof Slide>
+
+export const ExampleSlide: Story = {
+    args: {
+        mainImage: '/slides/1/main.jpg',
+        mainText:
+            'Целый вагон впечатлений и тележка эмоций: все проекты производства Иви в одном месте. Запрыгивай!',
+        titleText: 'Создано Иви',
+    },
+}
