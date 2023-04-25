df8aa5dff9102b1fa6c1a295435d42dc44d3629a
Dmitry Khodyrev <dooommm@mail.ru>
2023-04-25 17:00:12 +0800

    сменил шрифт на open sans

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import '@/styles/globals.scss'
 import '@/styles/vars.scss'
 import type { AppProps } from 'next/app'
 import { Roboto } from 'next/font/google'
+import { Open_Sans } from 'next/font/google'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { I18nProvider } from '@/context/i18n'
@@ -10,6 +11,12 @@ import Head from 'next/head'
 
 const roboto = Roboto({
     weight: ['100', '300', '400', '500', '700'],
+    style: ['normal', 'italic'],
+    subsets: ['latin'],
+})
+
+const openSans = Open_Sans({
+    weight: ['300', '400', '500', '700'],
     style: ['normal', 'italic'],
     subsets: ['latin'],
 })
@@ -27,7 +34,7 @@ export default function App({ Component, pageProps }: AppProps) {
                 <Layout>
                     <style jsx global>{`
                         html {
-                            font-family: ${roboto.style.fontFamily};
+                            font-family: ${openSans.style.fontFamily};
                         }
                     `}</style>
                     <Component {...pageProps} />
