0af81eafc5b8b94ab095636aaf49957edbfd6542
khanchenkov <daniilkhanchenkov@gmail.com>
2023-05-04 21:37:45 +0300

    Добавил кнопку для перехода в админку

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FooterNavigation from '../FooterNavigation/FooterNavigation'
 import FooterContacts from '../FooterContacts/FooterContacts'
 import FooterMobile from '../FooterMobile/FooterMobile'
 import useMediaQuery from '@/hooks/useMediaQuery'
 import styles from './Footer.module.scss'
+import { useRouter } from 'next/router'
 
 const Footer = () => {
     const matchesDesktopSize = useMediaQuery('(min-width: 1160px)')
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false)
+    const { push } = useRouter()
 
     return (
         <footer className={styles.footer}>
@@ -14,6 +17,14 @@ const Footer = () => {
             {matchesDesktopSize && (
                 <div className="container">
                     <FooterNavigation />
+                    {isLoggedIn && (
+                        <button
+                            className={styles.admin}
+                            onClick={() => push('/admin')}
+                        >
+                            Админ-панель
+                        </button>
+                    )}
                     <FooterContacts />
                 </div>
             )}
