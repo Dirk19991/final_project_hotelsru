b181e702c3073560a6352938ac010f29d515627e
khanchenkov <daniilkhanchenkov@gmail.com>
2023-05-15 17:58:43 +0300

    добаление списка фильмов в next api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,11 @@ import MediumCarousel from '@/components/MediumCarousel/MediumCarousel'
 import PromoButtons from '@/components/PromoButtons/PromoButtons'
 import Head from 'next/head'
 import DefaultCarousel from '@/stories/DefaultCarousel/DefaultCarousel'
+import { GetStaticProps } from 'next'
+import { FC } from 'react'
 
-export default function Home() {
+const Home: FC<any> = ({ dramas }) => {
+
     return (
         <>
             <Head>
@@ -18,6 +21,7 @@ export default function Home() {
             <PromoButtons />
             <Promo />
             <MediumCarousel />
+
             <DefaultCarousel
                 type="endpoint"
                 endpoint="http://localhost:3001/movies?year=2020"
@@ -30,4 +34,17 @@ export default function Home() {
             />
         </>
     )
+}
+
+export default Home
+
+export const getStaticProps: GetStaticProps = async () => {
+    const baseURL = process.env.VERCEL_URL ?? 'http://localhost:3000'
+
+    const response = await fetch(`${baseURL}/api/movies-list`)
+    const dramas = await response.json()
+
+    return {
+        props: { dramas },
+    }
 }
