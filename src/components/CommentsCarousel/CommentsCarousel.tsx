643bcf51837d019511c876e14dfe608b37921580
Anton <antoha_denisenkov@yandex.ru>
2023-05-28 22:36:52 +0500

    стр. фильма и комментарии

--- src/components/CommentsCarousel/CommentsCarousel.tsx
+++ src/components/CommentsCarousel/CommentsCarousel.tsx
@@ -2,26 +2,69 @@ import styles from './CommentsCarousel.module.scss'
 import Image from 'next/image'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation } from 'swiper'
-import { CSSProperties, useRef, useState } from 'react'
+import { CSSProperties, useEffect, useRef, useState } from 'react'
 import Modal from '../Modal/Modal'
 import { Button } from '@/stories/Button/ButtonStandard'
 import Comments from '../Comments/Comments'
 import commentsData from '../../data/mockDataComments.json'
 import CommentSlide from '@/stories/CommentSlide/CommentSlide'
+import { IMovie } from '@/types/ComponentProps/IMovie'
 
-const CommentsCarousel = ({}) => {
+const url = process.env.DEPLOY_API_URL 
+
+export type CommentType = {
+    id?: number
+    author: {
+        userTd: string
+        name: string
+    }
+    date: null | string
+    essenceId?: string
+    likes?: string
+    movieId?: string
+    text: string
+    comments?: CommentType[]
+    setCommentsRefresh: (value: boolean) => void
+    toggleIsAnswerOpen?: (value: boolean) => void
+}
+
+interface ICommentsCarousel {
+    film: IMovie
+    refreshComments: boolean
+    setCommentsRefresh: (value: boolean) => void
+}
+
+const CommentsCarousel = ({ film, refreshComments, setCommentsRefresh }: ICommentsCarousel) => {
     const [isModalOpen, setIsModalOpen] = useState(false)
     const prevRef = useRef(null)
     const nextRef = useRef(null)
+
+    const [comments, setComments] = useState<Array<CommentType>>([])
+
+    useEffect(() => {
+        fetch(`${url}/comments?movieId=${film.id}`)
+            .then((response) => {
+                if (response.ok) {
+                    return response.json()
+                }
+                throw new Error('Error')
+            })
+            .then((res) => {         
+                if (res.length) {
+                    setComments(res)
+                }
+            })
+            .catch((error) => setComments(JSON.parse(JSON.stringify(commentsData))))
+
+        setCommentsRefresh(false)
+    }, [film.id, refreshComments])
+
     return (
         <>
             <div className="container">
                 <div className={styles.wrapper}>
                     <div className={styles.top}>
-                        <h3
-                            className={styles.title}
-                            onClick={() => setIsModalOpen(true)}
-                        >
+                        <h3 className={styles.title} onClick={() => setIsModalOpen(true)}>
                             Комментарии
                         </h3>
                         <Button
@@ -65,36 +108,28 @@ const CommentsCarousel = ({}) => {
                                 nextEl: nextRef.current,
                             }}
                         >
-                            {commentsData.map((comment) => (
+                            {comments.map((comment) => (
                                 <SwiperSlide key={comment.id}>
                                     <CommentSlide
-                                        author={comment.author}
-                                        date={comment.date}
+                                        author={comment.author.name}
+                                        date={comment.date!}
                                         text={comment.text}
                                     />
                                 </SwiperSlide>
                             ))}
                         </Swiper>
                         <div className={styles.prevButton} ref={prevRef}>
-                            <Image
-                                fill
-                                src="/icons/arrowLeft.svg"
-                                alt="arrowLeft"
-                            />
+                            <Image fill src="/icons/arrowLeft.svg" alt="arrowLeft" />
                         </div>
                         <div className={styles.nextButton} ref={nextRef}>
-                            <Image
-                                fill
-                                src="/icons/arrowRight.svg"
-                                alt="arrowRight"
-                            />
+                            <Image fill src="/icons/arrowRight.svg" alt="arrowRight" />
                         </div>
                     </div>
                 </div>
             </div>
             {isModalOpen && (
                 <Modal toggle={setIsModalOpen}>
-                    <Comments />
+                    <Comments comments={comments} film={film} setCommentsRefresh={setCommentsRefresh} />
                 </Modal>
             )}
         </>
---
+++ src/util/formatDate.ts
@@ -0,0 +1,12 @@
+export function FormatDate(date: string) {
+    
+    const dateFormat = new Date(date)
+
+    const day = dateFormat.getDate();
+    const monthNames = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+    const monthIndex = dateFormat.getMonth();
+    const monthName = monthNames[monthIndex];
+    const year = dateFormat.getFullYear(); 
+
+    return `${day} ${monthName} ${year}`
+}
