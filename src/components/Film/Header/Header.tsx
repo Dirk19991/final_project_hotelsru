45b180b59270d8040b757a6edef48d02c0f46792
Dmitry Khodyrev <dooommm@mail.ru>
2023-05-29 21:07:19 +0800

    исправлены ошибки которые выявились при проверке линтером, два хелпера перенесены в компоненты чтоб избежать вызова в них хуков

--- src/components/Film/Header/Header.tsx
+++ src/components/Film/Header/Header.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react'
 import styles from './Header.module.scss'
 import { IMovie } from '@/types/ComponentProps/IMovie'
-import toHoursAndMinutes from '@/util/toHoursAndMinutes'
+
 import FilmBreadcrumbs from '../Breadcrumbs/Breadcrumbs'
 import cn from 'classnames'
 import { useTranslation } from 'next-i18next'
@@ -10,17 +10,23 @@ interface IHeader {
     film: IMovie
 }
 
-const Header: FC<IHeader> = ({ 
-    film: { year, type, nameRu, nameEn, genres, duration },
-}) => {
+const Header: FC<IHeader> = ({ film: { year, type, nameRu, nameEn, genres, duration } }) => {
+    function toHoursAndMinutes(totalMinutes: number) {
+        const hours = Math.floor(totalMinutes / 60)
+        const minutes = totalMinutes % 60
+
+        return i18n.language === 'en'
+            ? `${hours} h.${minutes > 0 ? ` ${minutes} m.` : ''}`
+            : `${hours} ч.${minutes > 0 ? ` ${minutes} мин.` : ''}`
+    }
+
     const { t, i18n } = useTranslation(['film'])
     const time = toHoursAndMinutes(duration)
 
     return (
         <div className={cn(styles.wrapper, {})}>
             <h2 className={styles.name}>
-                {i18n.language === 'en' ? nameEn : nameRu} (
-                {t(type as 'movie' | 'tv-series' | 'cartoon')} {year})
+                {i18n.language === 'en' ? nameEn : nameRu} ({t(type as 'movie' | 'tv-series' | 'cartoon')} {year})
             </h2>
             <div className={styles.production}>
                 <span className={styles.production__year}>{year}</span>
--- src/components/Person/Person.tsx
+++ src/components/Person/Person.tsx
@@ -4,11 +4,52 @@ import { useEffect, useState } from 'react'
 import PersonFilm from '../PersonFilm/PersonFilm'
 import { useRouter } from 'next/router'
 import { IPerson } from '@/types/ComponentProps/IMovie'
-import getFilmWord from '@/util/getFilmWord'
 import useMediaQuery from '@/hooks/useMediaQuery'
 import { useTranslation } from 'next-i18next'
 
 const Person = () => {
+    function getFilmWord(number: number | null) {
+        if (number === null) {
+            return
+        }
+
+        let lastTwoDigits = 0
+        if (number >= 10) {
+            lastTwoDigits = +number.toString().slice(-2)
+        }
+
+        if (lastTwoDigits >= 11 && lastTwoDigits <= 14) {
+            return i18n.language === 'en' ? 'movies' : 'фильмов'
+        }
+
+        const stringifiedNum = number.toString()
+        const lastDigit = +stringifiedNum[stringifiedNum.length - 1]
+
+        let filmWord = ''
+        switch (lastDigit) {
+            case 1:
+                filmWord = i18n.language === 'en' ? 'movie' : 'фильм'
+                break
+            case 2:
+            case 3:
+            case 4:
+                filmWord = i18n.language === 'en' ? 'movies' : 'фильма'
+                break
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 0:
+                filmWord = i18n.language === 'en' ? 'movies' : 'фильмов'
+                break
+            default:
+                break
+        }
+
+        return filmWord
+    }
+
     const isTab = useMediaQuery('(max-width: 1160px)')
     const router = useRouter()
 
@@ -24,9 +65,7 @@ const Person = () => {
     const name = actorData && actorData.name
     const enName = actorData && actorData.enName
     const numberOfFilms = actorData && actorData.movies.length
-    const filmWord = numberOfFilms
-        ? getFilmWord(numberOfFilms)
-        : getFilmWord(null)
+    const filmWord = numberOfFilms ? getFilmWord(numberOfFilms) : getFilmWord(null)
 
     const sortedMovies = actorData?.movies.sort((a, b) => b.year - a.year)
 
@@ -34,29 +73,15 @@ const Person = () => {
         <div className="container">
             <div className={styles.wrapper}>
                 <div onClick={() => router.back()} className={styles.back}>
-                    <Image
-                        width={15}
-                        height={15}
-                        src="/icons/arrowLeft.svg"
-                        alt="arrowLeft"
-                    />
+                    <Image width={15} height={15} src="/icons/arrowLeft.svg" alt="arrowLeft" />
                     <div>{t('back')}</div>{' '}
                 </div>
                 <div className={styles.mainInfo}>
                     <div className={styles.photo}>
-                        <Image
-                            layout="fill"
-                            objectFit="cover"
-                            src={actorData.photo}
-                            alt="photo"
-                        />
+                        <Image layout="fill" objectFit="cover" src={actorData.photo} alt="photo" />
                     </div>
-                    <div className={styles.ruName}>
-                        {i18n.language === 'en' ? enName : name}
-                    </div>
-                    <div className={styles.enName}>
-                        {i18n.language === 'ru' && enName}
-                    </div>
+                    <div className={styles.ruName}>{i18n.language === 'en' ? enName : name}</div>
+                    <div className={styles.enName}>{i18n.language === 'ru' && enName}</div>
                     <div className={styles.filmography}>
                         {t('fullFilmography')}{' '}
                         <span>
@@ -64,10 +89,7 @@ const Person = () => {
                         </span>
                     </div>
                     <div className={isTab ? styles.tabFilms : styles.films}>
-                        {sortedMovies &&
-                            sortedMovies.map((film) => (
-                                <PersonFilm key={film.id} film={film} />
-                            ))}
+                        {sortedMovies && sortedMovies.map((film) => <PersonFilm key={film.id} film={film} />)}
                     </div>
                 </div>
             </div>
