1f5921e4dd58a72aafd23513cc0992405fc8c1e8
khanchenkov <daniilkhanchenkov@gmail.com>
2023-06-02 19:20:13 +0300

    проправлены большая часть тестов

--- src/tests/components/MediumCarousel.test.tsx
+++ src/tests/components/MediumCarousel.test.tsx
@@ -1,13 +1,21 @@
 import MediumCarousel from '@/components/MediumCarousel/MediumCarousel'
 import { render } from '@testing-library/react'
+import data from '@/data/top10Movies.json'
+
+jest.mock('react-i18next', () => ({
+    useTranslation: () => {
+        return {
+            t: (str: string) => str,
+            i18n: {
+                changeLanguage: () => new Promise(() => {}),
+            },
+        }
+    },
+}))
 
 jest.mock('swiper/react', () => ({
-    Swiper: ({ children }: any) => (
-        <div data-testid="MediumCarousel-testId">{children}</div>
-    ),
-    SwiperSlide: ({ children }: any) => (
-        <div data-testid="MediumCarouselSlide-testId">{children}</div>
-    ),
+    Swiper: ({ children }: any) => <div data-testid="MediumCarousel-testId">{children}</div>,
+    SwiperSlide: ({ children }: any) => <div data-testid="MediumCarouselSlide-testId">{children}</div>,
 }))
 
 jest.mock('swiper', () => ({
@@ -19,8 +27,7 @@ jest.mock('swiper', () => ({
 
 describe('Testing medium carousel', () => {
     it('renders determined amount of slides', () => {
-        const view = render(<MediumCarousel />)
-
+        const view = render(<MediumCarousel data={data.movies} />)
         const slides = view.getAllByTestId('MediumCarouselSlide-testId')
         expect(slides.length).toBe(10)
     })
